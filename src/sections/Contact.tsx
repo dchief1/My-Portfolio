7999201a6b80d2caeff623930ccaf68cf0b47d29
kelechi <igwesiemmanuel4@gmail.com>
2025-12-07 16:50:49 +0100

    update

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -15,11 +15,11 @@ export const ContactSection = () => {
           <div className="flex flex-col gap-8 md:gap-16 items-center md:flex-row md:text-left">
             <div>
               <h2 className="font-serif text-2xl md:text-3xl">
-                Let's create something amazing together
+                Let&apos;s create something amazing together
               </h2>
               <p className="text-sm mt-2 md:text-base">
-                Ready to bring your next project to life? Let's connect and discuss how I can
-                help you achieve your goals.
+                Ready to bring your next project to life? Let&apos;s connect and discuss how I
+                can help you achieve your goals.
               </p>
             </div>
             <div>
